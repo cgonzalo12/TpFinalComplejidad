b964c6a42bf4261533549c92d9e5991af46dc5fa
cgonzalo12 <cap.gonzalo12@gmail.com>
2024-06-28 21:38:23 -0300

    1ra entrega

---
+++ Estrategia.cs
@@ -0,0 +1,129 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Numerics;
+using System.Security.Cryptography.X509Certificates;
+using System.Windows.Forms;
+using tp1;
+
+namespace tpfinal
+{
+
+	public class Estrategia
+	{
+	
+		public String Consulta1(List<string> datos)
+		{
+            List<Dato> collected = new List<Dato>();
+            int n = datos.Count;
+
+            List<Dato> collected1 = new List<Dato>();
+            int n1 = datos.Count;
+
+            // Crear un objeto Stopwatch para medir el tiempo con heap
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            BuscarConHeap(datos, 5, collected);
+            stopwatch.Stop();
+            // Obtener el tiempo transcurrido con heap
+            TimeSpan elapsedTime = stopwatch.Elapsed;
+
+            // Crear un objeto Stopwatch para medir el tiempo con otro
+            Stopwatch stopwatch1 = new Stopwatch();
+            stopwatch1.Start();
+            BuscarConOtro(datos, 5, collected);
+            stopwatch1.Stop();
+            // Obtener el tiempo transcurrido con otro
+            TimeSpan elapsedTime1 = stopwatch1.Elapsed;
+
+            string result = $"T({n}) = {elapsedTime.TotalMilliseconds} ms Con Heap";
+            result =result+ $"----- T({n}) = {elapsedTime1.TotalMilliseconds} ms Con otro";
+            return result;
+        }
+
+
+		public String Consulta2(List<string> datos)
+		{
+            Heap heap = new Heap();
+            List<Dato> collected = new List<Dato>();
+            collected=BuscarConHeap(datos, 5, collected);
+            string result;
+            result=heap.caminoHojaI(collected);
+            return result;
+        }
+
+		
+
+		public String Consulta3(List<string> datos)
+		{
+            List<Dato> collected = new List<Dato>();
+            collected=BuscarConHeap(datos,5, collected);
+            Heap heap = new Heap();
+            return heap.porNiveles(collected);
+        }
+
+
+
+        public List<Dato> BuscarConOtro(List<string> datos, int cantidad, List<Dato> collected)
+        {
+            List<Dato> aux = new List<Dato>();
+            List<Dato> respuesta = new List<Dato>();
+            Dato DatoAux;
+            aux = ParseToDato(datos);
+            aux=reordenarList(aux);
+            for (int i = 0; i <= cantidad-1; i++)
+            {
+                DatoAux = aux[i];
+                respuesta.Add(DatoAux);
+            }
+            return respuesta;
+        }
+
+        
+        public List<Dato> BuscarConHeap(List<string> datos, int cantidad, List<Dato> collected)
+        {
+            List<Dato> aux = new List<Dato>();
+            Dato DatoAux;
+            aux= ParseToDato(datos);
+
+            Heap heap = new Heap();
+            foreach (Dato item in aux)
+            {
+                heap.Add(item);
+            }
+            for (int i = 1; i <= cantidad; i++)
+            {
+                DatoAux = heap.deleteMax();
+                collected.Add(DatoAux);
+            }
+            return collected;
+        }
+
+        public static List<Dato> ParseToDato(List<string> listaDeStrings)
+        {
+            
+            List<Dato> collected= new List<Dato>();
+            foreach (var str in listaDeStrings)
+            {
+                var datoExistente = collected.FirstOrDefault(d => d.texto == str);
+
+                if (datoExistente != null)
+                {
+                    datoExistente.ocurrencia++;
+                }
+                else
+                {
+                    collected.Add(new Dato(1, str));
+                }
+            }
+
+            return collected;
+        }
+        public static List<Dato> reordenarList(List<Dato> lista)
+        {
+            return lista.OrderByDescending(d => d.ocurrencia).ToList();
+        }
+
+    }
+}
---
+++ Heap.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tpfinal
+{
+    internal class Heap
+    {
+        private List<Dato> heap;
+        private int tamanio;
+        public Heap()
+        {
+            heap = new List<Dato>();
+            Dato fals = new Dato(999, "asd");
+            heap.Add(fals);
+        }
+
+        public int getTamanio()
+        {
+            return tamanio = heap.Count - 1;
+        }
+        public List<Dato> getHeap()
+        {
+            return heap;
+        }
+        public Dato raiz()
+        {
+            return heap[1];
+        }
+        public void imprimirHeap()
+        {
+            for (int i = 1; i < heap.Count; i++)
+            {
+                Console.WriteLine(heap[i].ocurrencia + " ");
+            }
+        }
+        public void Add(Dato item)
+        {
+            heap.Add(item);
+            FiltradoHaciaArriba(getTamanio());
+        }
+        public void FiltradoHaciaArriba(int posicion)
+        {
+            while (posicion > 1)
+            {
+                int padr = posicion / 2;
+                Dato hijo = heap[posicion];
+                Dato padre = heap[padr];
+                if (hijo.ocurrencia > padre.ocurrencia)
+                {
+                    intercambio(posicion, padr);
+                    FiltradoHaciaArriba(padr);
+                }
+                else
+                {
+                    break;
+                }
+
+
+            }
+        }
+        public void filtradoHaciaAbajo(int posicion)
+        {
+            int tamanio = heap.Count;
+            while (posicion < tamanio)
+            {
+                int HIzquierdo = 2 * posicion ; 
+                int HDerecho = 2 * posicion +1;   
+                int posicionAux = posicion;
+                if (HIzquierdo < tamanio && heap[HIzquierdo].ocurrencia > heap[posicionAux].ocurrencia)
+                {
+                    posicionAux = HIzquierdo;
+                }
+                if (HDerecho < tamanio && heap[HDerecho].ocurrencia > heap[posicionAux].ocurrencia)
+                {
+                    posicionAux = HDerecho;
+                }
+                if (posicionAux == posicion)
+                {
+                    break; 
+                }
+
+                intercambio(posicion, posicionAux);
+                posicion = posicionAux;
+            }
+        }
+        public void intercambio(int posicionA, int posicionB)
+        {
+            Dato dato = heap[posicionA];
+            Dato dato2 = heap[posicionB];
+            heap[posicionA] = dato2;
+            heap[posicionB] = dato; 
+        }
+
+        public Dato deleteMax()
+        {
+            int tamanio=getTamanio();
+            Dato auxMax = heap[1];
+            intercambio(1, tamanio - 1);
+            heap.RemoveAt(tamanio - 1);
+            filtradoHaciaAbajo(1);
+            Console.WriteLine(auxMax.ToString());
+            return auxMax;
+        }
+        public string caminoHojaI(List<Dato> collected)
+        {
+            Dato datoAux = new Dato(99999,"");
+            collected.Insert(0, datoAux);
+            int tamanio = collected.Count-1;
+            int pos = 1;
+            int hijoI=pos*2;
+            string camino = "Camino al hijo mas izquierdo: ";
+            camino=camino+ collected[pos].ToString()+"//";
+            while (hijoI<=tamanio)
+            {
+                camino=camino + collected[hijoI].ToString()+"//";
+                hijoI=hijoI*2;
+            }
+            return camino;
+        }
+        
+        public string porNiveles(List<Dato> collected)
+        {
+            Dato datoAux = new Dato(99999,"");
+            collected.Insert(0, datoAux);
+            if (collected.Count <= 1)
+                return "Heap vacío";
+
+            string camino = "";
+            int nivel = 0;
+            int cantidadPorNivel = 1;
+            int contador = 0;
+
+            camino += $"Nivel {nivel}:";
+
+            for (int i = 1; i < collected.Count; i++)
+            {
+                camino += collected[i].ToString() + " ";
+                contador++;
+
+                if (contador == cantidadPorNivel)
+                {
+                    nivel++;
+                    cantidadPorNivel *= 2;
+                    contador = 0;
+                    if (i + 1 < collected.Count)
+                        camino += $"\nNivel {nivel}:";
+                }
+            }
+
+            return camino;
+
+        }
+
+    }
+}
